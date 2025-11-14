f0935e9de283ce31791731b7e50095805031607d
JaeWook Yoo <jwyoo9932@gmail.com>
2025-11-14 20:46:03 +0900

    uart 추가

---
+++ 13.uart/13.uart.srcs/sim_1/new/tb_uart_tx.v
@@ -0,0 +1,48 @@
+`timescale 1ns / 1ps
+
+module tb_uart_tx;
+    reg clk;
+    reg reset;
+    reg [7:0] tx_data;
+    reg tx_start;
+    wire tx;
+    wire tx_done;
+    wire tx_busy;
+
+    uart_tx  #(
+      .BPS(100_000_000)
+    )u_uart_tx(
+        .clk(clk),
+        .reset(reset),
+        .tx_data(tx_data),
+        .tx_start(tx_start),
+        .tx(tx),
+        .tx_done(tx_done),
+        .tx_busy(tx_busy)
+    );
+
+    initial clk = 0;
+    always #5 clk = ~clk;
+
+    initial begin
+        #100 reset = 1;
+          #50
+            reset = 0;
+            // '5' ==> 0x35 0011 0101
+            tx_data = 8'b00110101;
+            tx_start = 1'b1;
+          #20
+            tx_start = 1'b0;
+            wait (!tx_busy);  // tx data가 전송이 완료될 때까지 기다린다.
+          #50
+            // '7' ==> 0x37 0011 0111
+            tx_data = 8'b00110111;
+            tx_start = 1'b1;
+          #30
+            tx_start = 1'b0;
+            wait (!tx_busy);
+          #20
+            $display("UART TX test finish");
+            $finish;
+    end
+endmodule
---
+++ 13.uart/13.uart.srcs/sources_1/new/data_sender.v
@@ -0,0 +1,54 @@
+`timescale 1ns / 1ps
+
+module data_sender(
+    input clk,
+    input reset,
+    input start_trigger,
+    input [13:0] send_data,
+    input tx_busy,
+    input tx_done,
+    output reg tx_start,
+    output reg [7:0] tx_data
+    );
+
+    // CR, H, e, l, l, o, !, , J, W, Y, o, o, LF
+    reg [7:0] msg[0:13];
+    reg [3:0] r_data_cnt = 0;
+
+    initial begin
+        msg[0] = 8'h0D;
+        msg[1] = "H";
+        msg[2] = "e";
+        msg[3] = "l";
+        msg[4] = "l";
+        msg[5] = "o";
+        msg[6] = 8'h21;
+        msg[7] = 8'h20;
+        msg[8] = "J";
+        msg[9] = "W";
+        msg[10] = "Y";
+        msg[11] = "o";
+        msg[12] = "o";
+        msg[13] = 8'h0A;
+    end
+
+    always @(posedge clk, posedge reset) begin
+        if(reset) begin
+            tx_start <= 0;
+            r_data_cnt <= 0;
+        end else begin
+            if(start_trigger && !tx_busy) begin
+                tx_start <= 1'b1;
+                tx_data <= msg[r_data_cnt];
+
+                if(r_data_cnt == 13) begin
+                    r_data_cnt <= 0;
+                end else begin
+                    r_data_cnt <= r_data_cnt + 1;
+                end
+            end else begin
+                tx_start <= 1'b0;
+            end
+        end
+    end
+endmodule
